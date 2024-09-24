579f31898a45609fa404f84b7d28c2dcfc5f77e3
BskRao <saikiran1670@gmail.com>
2024-09-24 15:23:09 +1000

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -1,24 +1,47 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
+# Define the provider
+provider "aws" {
+  region = "ap-southeast-2"  # Specify the region
+}
+
+# Define an EC2 security group to allow SSH access
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Replace with your IP for more security
   }
 
-  required_version = ">= 1.2.0"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-provider "aws" {
-  region  = "us-west-2"
-}
+# Define EC2 instances
+resource "aws_instance" "example" {
+  count = 3  # Create 3 EC2 instances
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  ami           = "ami-0bfe89c8e3d79ad82"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"               # Instance type
+  key_name      = "LEO.pem"            # Replace with your key pair name
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
-    Name = "Terraform_Demo"
+    Name = "Terraform-EC2-Instance-${count.index + 1}"
   }
 }
+
+# Output public IPs of the instances
+output "instance_public_ips" {
+  description = "The public IPs of the EC2 instances"
+  value       = aws_instance.example[*].public_ip
+}
+
 
